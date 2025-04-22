f9e210a8012107f2005894a08be9afd009a64666
Murilo <md.henriques@outlook.com>
2025-04-21 21:17:27 -0300

    Componente Box

---
+++ src/components/Box.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+const props = defineProps<{
+    title: string
+}>()
+</script>
+
+<template>
+    <div class="box">
+      <div class="box-header">
+        <span class="title">{{ title }}</span>
+        <slot name="header-action" />
+      </div>
+      <slot />
+    </div>
+  </template>
+
+<style scoped>
+/* Estilização das caixas */
+.box {
+  flex-grow: 1;
+  min-height: 100%;
+  background-color: white;
+  overflow-y: auto;
+  position: relative;
+  /* Garante que .box-header fique limitada à .box */
+}
+
+/* Header da primeira caixa */
+.box-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  background-color: rgba(160, 160, 160, 0.997);
+  border-bottom: 2px solid #ddd;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  /* Para garantir que não ultrapasse a largura da .box */
+}
+</style>
--- src/components/ContentContainer.vue
+++ src/components/ContentContainer.vue
@@ -1,47 +1,52 @@
 <script lang="ts" setup>
 import type { Task } from '@/types/task';
 import TaskCard from './TaskCard.vue';
+import Box from './Box.vue';
 
 
 const props = defineProps<{
-    tasks: Task[]
+  tasks: Task[]
 }>()
 
 const emit = defineEmits<{
-    (e: 'add-task'): void
-    (e: 'task-updated'): void
-    (e: 'mark-completed', taskId: number, reward: { coins: number, xp: number }): void;
+  (e: 'add-task'): void
+  (e: 'task-updated'): void
+  (e: 'mark-completed', taskId: number, reward: { coins: number, xp: number }): void;
 }>()
 </script>
 
 <template>
-    <div class="content-container">
-      <div class="box">
-        <div class="box-header">
-          <span class="title">TAREFAS</span>
-          <button class="add-task-button" @click="emit('add-task')">+</button>
-        </div>
-        <TaskCard
-         v-for="task in tasks"
-         :key="task.id" 
-         :task="task" 
-         @task-updated="emit('task-updated')"
-         @mark-completed="(id, reward) => $emit('mark-completed', id, reward)"
-        />
-      </div>
-      <div class="box"></div>
-      <div class="box"></div>
-    </div>
+  <div class="content-container">
+    <Box title="TAREFAS">
+      <template #header-action>
+        <button class="add-task-button" @click="emit('add-task')">+</button>
+      </template>
+      <TaskCard
+       v-for="task in tasks" 
+       :key="task.id" 
+       :task="task" 
+       @task-updated="emit('task-updated')"
+       @mark-completed="(id, reward) => emit('mark-completed', id, reward)" />
+    </Box>
+    <Box title="OUTROS">
+      
+    </Box>
+    <Box title="MISSÕES">
+
+    </Box>
+  </div>
 </template>
 
 <style scoped>
 /* Container das caixas */
 .content-container {
   position: absolute;
-  top: 213px; /* distância abaixo do avatar/perfil */
+  top: 213px;
+  /* distância abaixo do avatar/perfil */
   left: 54px;
   right: 54px;
-  bottom: 54px; /* mantém uma margem inferior */
+  bottom: 54px;
+  /* mantém uma margem inferior */
   display: flex;
   justify-content: flex-start;
   gap: 20px;
@@ -49,32 +54,7 @@ const emit = defineEmits<{
   height: auto;
 }
 
-/* Estilização das caixas */
-.box {
-  flex-grow: 1;
-  min-height: 100%;
-  background-color: white;
-  overflow-y: auto;
-  position: relative;
-  /* Garante que .box-header fique limitada à .box */
-}
 
-/* Header da primeira caixa */
-.box-header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  padding: 10px;
-  box-sizing: border-box;
-  background-color: rgba(160, 160, 160, 0.997);
-  border-bottom: 2px solid #ddd;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  /* Para garantir que não ultrapasse a largura da .box */
-}
 
 /* Botão "+" */
 .add-task-button {
